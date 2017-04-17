74d4a73e4c192cc807fb3c97b29e73aa639e784d
Omegamark <grant.mark@gmail.com>
2017-04-16 18:17:53 -0600

    completed splitStrings

--- splitStrings.js
+++ splitStrings.js
@@ -4,12 +4,26 @@ function solution(str){
      // divide the string by the number
      var limiter = str.length/2;
      var splitString = str.match(/.{1,2}/g);
+   } else {
+     splitString = str.match(/.{1,2}/g);
+     var lastIndex = splitString.pop();
+     while (lastIndex.length < 2) {
+       lastIndex += '_';
+       console.log(lastIndex)
+       // Have last index be added to the array
+      // console.log(splitString)
+      // splitString.pop();
+      splitString.push(lastIndex)
+       console.log(splitString)
+     }
    }
    console.log(splitString);
+   return splitString;
    // else divide by the string
    // map the numbers to the indices of the characters
    // divide the characters into groups of 2 and push into an array
    // return the array
 }
 
-solution('fasdfads');
+
+solution('fasdfadsd');
