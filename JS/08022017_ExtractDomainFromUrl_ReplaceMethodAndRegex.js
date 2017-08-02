811be735e3ca51cba721a309afe4d3a8fa3b57ce
Omegamark <grant.mark@gmail.com>
2017-08-02 12:20:30 -0600

    did a little more practice with regex

---
+++ JS/08022017_ExtractDomainFromUrl_ReplaceMethodAndRegex.js
@@ -0,0 +1,17 @@
+function domainName(url) {
+  // Regex to remove everything but.
+  // Still need to better understand Regex
+  let solution = url
+    .replace("www.", "")
+    .replace("http://", "")
+    .replace("https://", "")
+    .replace(/\..*/, "");
+  return solution;
+}
+domainName("http://www.zombie-bites.com");
+
+// Best answer from Codewars
+// *** Study this answer since it uses pure Regex ***
+// function domainName(url) {
+//   return url.match(/(?:http(?:s)?:\/\/)?(?:w{3}\.)?([^\.]+)/i)[1];
+// }
